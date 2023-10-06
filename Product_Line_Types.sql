9e095b101aac7605f42a60bca3fc375ee5e7f4b6
Suravell <93171067+Suravell@users.noreply.github.com>
2023-10-06 11:35:02 -0600

    Create Product_Line_Types.sql

---
+++ Product_Line_Types.sql
@@ -0,0 +1,12 @@
+SELECT warehousecode, productline
+from products
+WHERE warehousecode = "a";
+SELECT warehousecode, productline
+from products
+WHERE warehousecode = "B";
+              SELECT warehousecode, productline
+              from products
+              WHERE warehousecode = "c";
+              SELECT warehousecode, productline
+              from products
+              WHERE warehousecode = "d";
