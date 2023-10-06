c5a8960201facd2c3357b45cb916b8a6b098371f
Suravell <93171067+Suravell@users.noreply.github.com>
2023-10-06 11:41:56 -0600

    Create Product_Line_Highest_Sale.sql

---
+++ Product_Line_Highest_Sale.sql
@@ -0,0 +1,6 @@
+select products.productLine, count(orderdetails.productCode) as no_of_sales
+from products 
+join orderdetails 
+on products.productCode = orderdetails.productCode
+group by products.productLine
+order by no_of_sales desc;
