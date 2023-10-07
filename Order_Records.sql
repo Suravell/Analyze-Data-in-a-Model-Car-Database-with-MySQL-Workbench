08218ab2fdf8705936341d3fb54d2554c341f53f
Suravell <93171067+Suravell@users.noreply.github.com>
2023-10-06 19:34:03 -0600

    Update Order_Records.sql

--- Order_Records.sql
+++ Order_Records.sql
@@ -1,3 +1,4 @@
+--Total number of orders recorded between 2003 and 2005.
 SELECT YEAR(orders.orderDate) AS Year, SUM(orderdetails.quantityOrdered) AS Total_orders
 FROM orders 
 JOIN orderdetails ON orders.orderNumber= orderdetails.orderNumber 
