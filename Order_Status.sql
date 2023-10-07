012edbc317e2f7888b90e4d07f5542268858070d
Suravell <93171067+Suravell@users.noreply.github.com>
2023-10-06 19:35:49 -0600

    Update Order_Status.sql

--- Order_Status.sql
+++ Order_Status.sql
@@ -1,3 +1,4 @@
+--How many products has been Shipped, cancelled, disputed, on hold, resolved, in process from the period of 2003 and 2005.
 SELECT status, COUNT(status) AS total_of_shipped
 FROM orders
 WHERE status = "Shipped"
