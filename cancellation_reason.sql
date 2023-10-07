a187f499a060cefbca8d02c37333d110b23fb628
Suravell <93171067+Suravell@users.noreply.github.com>
2023-10-06 19:48:07 -0600

    Update cancellation_reason.sql

--- cancellation_reason.sql
+++ cancellation_reason.sql
@@ -1,3 +1,4 @@
+--What led to the cancellation of the orders.
 select orders.status, orders.comments, customers.country
 FROM orders
 JOIN customers ON orders.customernumber = customers.customernumber
